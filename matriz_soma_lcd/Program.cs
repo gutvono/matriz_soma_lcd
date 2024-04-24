e7085e18baf550f4b0525f7402790714c4d8cdec
Gustavo <guvono4@gmail.com>
2024-04-24 15:11:21 -0300

    Add project files.

---
+++ matriz_soma_lcd/Program.cs
@@ -0,0 +1,50 @@
+﻿int tamanho = 3;
+int[,] matriz = new int[tamanho, tamanho];
+int[] soma_linhas = new int[tamanho];
+int[] soma_colunas = new int[tamanho];
+int diagonal1 = 0, diagonal2 = 0, i_decrescente = tamanho;
+
+for (int l = 0; l < tamanho; l++)
+{
+    Console.WriteLine();
+    for (int c = 0; c < tamanho; c++)
+    {
+        matriz[l, c] = new Random().Next(1, 10);
+        Console.Write($"{matriz[l, c]} ");
+    }
+}
+
+Console.WriteLine("\n\nSOMA DE LINHAS:");
+for (int l = 0; l < tamanho; l++)
+{
+    for (int c = 0; c < tamanho; c++)
+    {
+        soma_linhas[l] += matriz[l, c];
+    }
+    Console.Write($"{soma_linhas[l]} ");
+}
+
+Console.WriteLine("\n\nSOMA DE COLUNAS:");
+for (int c = 0; c < tamanho; c++)
+{
+    for (int l = 0; l < tamanho; l++)
+    {
+        soma_colunas[c] += matriz[l, c];
+    }
+    Console.Write($"{soma_colunas[c]} ");
+}
+
+Console.WriteLine("\n\nSOMA DE DIAGONAIS:");
+for (int l = 0; l < tamanho; l++)
+{
+    diagonal1 += matriz[l, l];
+}
+
+int aux_l = 0;
+for (int c = tamanho - 1; c >= 0; c--)
+{
+    diagonal2 += matriz[aux_l, c];
+    aux_l++;
+}
+Console.WriteLine("Diagonal 1 = " + diagonal1);
+Console.WriteLine("Diagonal 2 = " + diagonal2);
